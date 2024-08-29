5cf37aec162aadf4c4a9ad743e5f4d1c225c6b5e
juhoo <juho.ouli@metropolia.fi>
2024-08-29 15:05:38 +0300

    1.

--- Moduuli 4/Moduuli 4.3.py
+++ Moduuli 4/Moduuli 4.3.py
@@ -1,2 +1,9 @@
-a = float(input("Anna minulle lukuja. Tyhjä merkki luvun viimeisenä merkkinä lopettaa kyselyn "
-                "ja tulostaa pienimmän sekä suurimman syötetyn luvun"))
+luvut = []
+while True:
+    a = input("Anna minulle lukuja. Tyhjä merkki lopettaa kyselyn "
+                    "ja tulostaa pienimmän sekä suurimman syötetyn luvun ")
+    if a != "":
+        luvut.append(a)
+    else:
+        print("Syötetyistä luvuista pienin on " + str(min(luvut))) + " ja pienin on " + str(max(luvut))
+        break
--- Moduuli 4/Moduuli 4.6.py
+++ Moduuli 4/Moduuli 4.6.py
@@ -1,3 +1,12 @@
-#randomilla N pistettä, vaikka 10 000 pistettä
-#jos pisteen x ja y arvoilla x^2 + y^2 < 1, niin piste on sisällä =n
-#lopuksi pii on (4*n)/N
+import random
+
+sisalla = 0
+kerrat = 1
+yht = int(input("Kuinka monta pistettä suoritetaan randomilla: "))
+while kerrat <= yht:
+    x = random.random()
+    y = random.random()
+    if (x**2)+(y**2)<1:
+        sisalla = sisalla + 1
+    kerrat = kerrat + 1
+print("Algoritmi laski piin likiarvoksi " + str((4*sisalla)/yht))
