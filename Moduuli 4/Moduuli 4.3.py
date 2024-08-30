72402d09d6b353b78fbec1c64e66979706d0e6e6
juhoo <juho.ouli@metropolia.fi>
2024-08-30 10:38:35 +0300

    1.

--- Moduuli 4/Moduuli 4.3.py
+++ Moduuli 4/Moduuli 4.3.py
@@ -2,11 +2,8 @@ luvut = []
 while True:
     a = input("Anna minulle lukuja. Tyhjä merkki lopettaa kyselyn "
                     "ja tulostaa pienimmän sekä suurimman syötetyn luvun ")
-    if isinstance(a, float) or isinstance(a, int):
+    if a!="":
             luvut.append(float(a))
     else:
-        if a != "":
-            print("Syötetyistä luvuista pienin on " + str(min(luvut)) + " ja suurin on " + str(max(luvut)))
-            break
-        else:
-            a = input("Anna luku")
+        print("Syötetyistä luvuista pienin on " + str(min(luvut)) + " ja suurin on " + str(max(luvut)))
+        break
